6ecff173586ba26742c0764f00e4fabfa83d18c9
‪Moaz Omar‬‏ <112810706+Mo-code7@users.noreply.github.com>
2026-07-04 23:35:47 +0300

    Delete models directory

--- models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import restaurant_order
-from . import restaurant_order_line
-from . import res_config_settings
-from . import restaurant_sale_report
-from . import restaurant_dashboard
